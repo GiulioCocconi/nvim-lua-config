5e4ab1e11de4337b34c1ecaf6434d0c60586571f
Giulio Cocconi <coccogiulio8@gmail.com>
2023-05-03 22:37:04 +0200

    Dashboard fixed

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -2,32 +2,39 @@ local M = {}
 
 local utils = require('core.utils')
 local icons = utils.icons
-local dashboard = require('dashboard')
 
 function M.config()
-	--vim.g.dashboard_default_executive ='telescope'
-	dashboard.custom_header = {
-		'',
-		'  ██████╗       ██████╗     █╗  ███████╗     ███╗   ██╗ ██╗   ██╗ ██╗ ███╗   ███╗ ',
-		' ██╔════╝      ██╔════╝     ╚╝  ██╔════╝     ████╗  ██║ ██║   ██║ ██║ ████╗ ████║ ',
-		' ██║  ███╗     ██║              ███████╗     ██╔██╗ ██║ ██║   ██║ ██║ ██╔████╔██║ ',
-		' ██║   ██║     ██║              ╚════██║     ██║╚██╗██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║ ',
-		' ╚██████╔╝ ██╗ ╚██████╗ ██╗     ███████║     ██║ ╚████║  ╚████╔╝  ██║ ██║ ╚═╝ ██║ ',
-		'  ╚═════╝  ╚═╝  ╚═════╝ ╚═╝     ╚══════╝     ╚═╝  ╚═══╝   ╚═══╝   ╚═╝ ╚═╝     ╚═╝ ',
-		'', '',
-	}
+	--vim.g.dashboard_default_executive ='telescope
+	require("dashboard").setup{
+		config = {
+			header = {
+				'',
+				'  ██████╗       ██████╗     █╗  ███████╗     ███╗   ██╗ ██╗   ██╗ ██╗ ███╗   ███╗ ',
+				' ██╔════╝      ██╔════╝     ╚╝  ██╔════╝     ████╗  ██║ ██║   ██║ ██║ ████╗ ████║ ',
+				' ██║  ███╗     ██║              ███████╗     ██╔██╗ ██║ ██║   ██║ ██║ ██╔████╔██║ ',
+				' ██║   ██║     ██║              ╚════██║     ██║╚██╗██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║ ',
+				' ╚██████╔╝ ██╗ ╚██████╗ ██╗     ███████║     ██║ ╚████║  ╚████╔╝  ██║ ██║ ╚═╝ ██║ ',
+				'  ╚═════╝  ╚═╝  ╚═════╝ ╚═╝     ╚══════╝     ╚═╝  ╚═══╝   ╚═══╝   ╚═╝ ╚═╝     ╚═╝ ',
+				'', '',
+			},
 
-	 dashboard.custom_center = {
-		{icon = icons.fileBg,		desc = 'Find File',			action = 'Telescope find_files hidden=true'},
-		{icon = icons.fileCopy,		desc = 'Recents',			action = 'Telescope oldfiles hidden=true'},
-		{icon = icons.timer,		desc = 'Load Last Session', action = 'SessionLoad'},
-		{icon = icons.container,	desc = 'Sync Plugins',		action = 'PackerSync'},
-		{icon = icons.error,		desc = 'Exit ',				action = 'exit'},
-	}
 
-	dashboard.custom_footer = {
-		'',
-		"Config loaded :)"
+
+			center = {
+				{icon = icons.fileBg,		desc = 'Find File',			action = 'Telescope find_files hidden=true'},
+				{icon = icons.fileCopy,		desc = 'Recents',			action = 'Telescope oldfiles hidden=true'},
+				{icon = icons.timer,		desc = 'Load Last Session', action = 'SessionLoad'},
+				{icon = icons.container,	desc = 'Sync Plugins',		action = 'PackerSync'},
+				{icon = icons.error,		desc = 'Exit ',				action = 'exit'},
+			},
+
+
+			footer = {
+				'',
+				"Config loaded :)"
+			},
+		}
+
 	}
 end
 
