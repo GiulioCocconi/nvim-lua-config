ada613cccccdfae2c1f1a53918f6bb6ba8ac324f
Giulio Cocconi <coccogiulio8@gmail.com>
2023-05-03 21:51:07 +0200

    Removed previous snippet engine

--- lua/plugins/snips.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-function M.config()
-	vim.g.UltiSnipsExpandTrigger="<c-space>"
-	vim.g.UltiSnipsJumpForwardTrigger="<c-b>"
-	vim.g.UltiSnipsJumpBackwardTrigger="<c-z>"
-end
-
-return M
